810f4190893d848b323c0b33b6f4ae45aa46ba70
xbwer <maxxbw1992@gmail.com>
2019-12-04 02:27:02 +0800

    update

--- methodFinder/MethodFinder.java
+++ methodFinder/MethodFinder.java
@@ -2,6 +2,7 @@ package paToolkit4Java.methodFinder;
 
 import org.eclipse.jdt.core.dom.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -9,98 +10,126 @@ import java.util.List;
 
 import static javaToolkit.lib.utils.FileUtil.getFileContent;
 
-
 public class MethodFinder {
 
+	public static class MethodNode {
 
-    public static class MethodNode {
+		MethodDeclaration node;
+		String methodName;
+		int startLineNum;
+		int endLineNum;
 
-        MethodDeclaration node;
-        String methodName;
-        int startLineNum;
-        int endLineNum;
+		public MethodNode(String methodName, MethodDeclaration node, int startLineNum, int endLineNum) {
+			this.methodName = methodName;
+			this.node = node;
+			this.startLineNum = startLineNum;
+			this.endLineNum = endLineNum;
+		}
 
-        public MethodNode(String methodName, MethodDeclaration node, int startLineNum, int endLineNum) {
-            this.methodName = methodName;
-            this.node = node;
-            this.startLineNum = startLineNum;
-            this.endLineNum = endLineNum;
-        }
+		public boolean equals(MethodNode obj) {
+			if (this.node.toString().equals(obj.node.toString())) {
+				return true;
+			}
+			return false;
+		}
 
-        public boolean equals(MethodNode obj) {
-            if (this.node.toString().equals(obj.node.toString())) {
-                return true;
-            }
-            return false;
-        }
+		public int getStartLineNum() {
+			return startLineNum;
+		}
 
-        public int getStartLineNum() {
-            return startLineNum;
-        }
-        
-        public String getMethodName() {
-            return methodName;
-        }
+		public String getMethodName() {
+			return methodName;
+		}
 
-        public void setEndLineNum(int endLineNum) {
-            this.endLineNum = endLineNum;
-        }
+		public void setEndLineNum(int endLineNum) {
+			this.endLineNum = endLineNum;
+		}
 
-        public MethodDeclaration getNode() {
-            return node;
-        }
-    }
+		public MethodDeclaration getNode() {
+			return node;
+		}
+	}
 
-    public MethodNode getMethodforGivenLineNum(String srcFilePath, int linenum) {
-        ASTParser parser = ASTParser.newParser(AST.JLS8);
+	public MethodNode getMethodforGivenLineNum(String srcFilePath, int linenum) {
+		ASTParser parser = ASTParser.newParser(AST.JLS8);
 
-        char[] fileContent = null;
-        try {
-            fileContent = getFileContent(srcFilePath).toCharArray();
-        } catch (IOException e) {
-            System.out.printf("getMethodforGivenLineNum-getFileContent failed!\n%s", srcFilePath);
-            e.printStackTrace();
-            return null;
-        }
+		char[] fileContent = null;
+		try {
+			fileContent = getFileContent(srcFilePath).toCharArray();
+		} catch (IOException e) {
+			System.out.printf("getMethodforGivenLineNum-getFileContent failed!\n%s", srcFilePath);
+			e.printStackTrace();
+			return null;
+		}
 
-        parser.setSource(fileContent);
+		parser.setSource(fileContent);
 
-        CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+		CompilationUnit cu = (CompilationUnit) parser.createAST(null);
 
-        List<MethodNode> methodNodeList = new ArrayList<MethodNode>();
+		List<MethodNode> methodNodeList = new ArrayList<MethodNode>();
 
-        cu.accept(new ASTVisitor() {
-            @Override
-            public boolean visit(MethodDeclaration node) {
-                SimpleName methodName = node.getName();
+		cu.accept(new ASTVisitor() {
+			@Override
+			public boolean visit(MethodDeclaration node) {
+				SimpleName methodName = node.getName();
 
-                int startLineNum = cu.getLineNumber(node.getStartPosition());
+				int startLineNum = cu.getLineNumber(node.getStartPosition());
 
-                int endLineNum = cu.getLineNumber(node.getStartPosition() + node.getLength());
+				int endLineNum = cu.getLineNumber(node.getStartPosition() + node.getLength());
 
-                methodNodeList.add(new MethodNode(methodName.toString(), node, startLineNum, endLineNum));
-                return false;
-            }
-        });
+				methodNodeList.add(new MethodNode(methodName.toString(), node, startLineNum, endLineNum));
+				return false;
+			}
+		});
 
-        MethodNode targetMethod = null;
-        for (int i = 0; i < methodNodeList.size(); i++) {
-            MethodNode m = methodNodeList.get(i);
-            // from begin to the end the first method that
-            if (linenum >= m.startLineNum && linenum<=m.endLineNum) {
-                targetMethod = m;
-            }
-        }
-        return targetMethod;
-    }
+		MethodNode targetMethod = null;
+		for (int i = 0; i < methodNodeList.size(); i++) {
+			MethodNode m = methodNodeList.get(i);
+			// from begin to the end the first method that
+			if (linenum >= m.startLineNum && linenum <= m.endLineNum) {
+				targetMethod = m;
+			}
+		}
+		return targetMethod;
+	}
 
-    public static void main(String[] args) {
-        MethodFinder mf = new MethodFinder();
-        MethodNode mn = mf.getMethodforGivenLineNum("/home/bowen/Desktop/research/Transformation4J/java-project/ct4j/src/main/java/paToolkit4Java/methodFinder/MethodFinder.java", 60);
-        System.out.println(mn.getNode().toString());
+	public List<MethodDeclaration> getAllMethodforGivenSRCFile(File srcFile) {
+		ASTParser parser = ASTParser.newParser(AST.JLS8);
 
-    }
+		char[] fileContent = null;
+		try {
+			fileContent = getFileContent(srcFile.getAbsolutePath()).toCharArray();
+		} catch (IOException e) {
+			System.out.printf("getAllMethodforGivenSRCFile-getFileContent failed!\n%s", srcFile.getAbsolutePath());
+			e.printStackTrace();
+			return null;
+		}
 
+		parser.setSource(fileContent);
+
+		CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+
+		List<MethodDeclaration> methodNodeList = new ArrayList<MethodDeclaration>();
+
+		cu.accept(new ASTVisitor() {
+			@Override
+			public boolean visit(MethodDeclaration node) {
+
+				methodNodeList.add(node);
+				return false;
+			}
+		});
+
+		return methodNodeList;
+	}
+
+	public static void main(String[] args) {
+		MethodFinder mf = new MethodFinder();
+		MethodNode mn = mf.getMethodforGivenLineNum(
+				"/home/bowen/Desktop/research/Transformation4J/java-project/ct4j/src/main/java/paToolkit4Java/methodFinder/MethodFinder.java",
+				60);
+		System.out.println(mn.getNode().toString());
+
+	}
 
 }
-
